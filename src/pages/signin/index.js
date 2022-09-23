d8c18be235a80e03f186eb5597d885e15b09bda5
Shibghotul Islam <m.shibghotul.islam@accenture.com>
2022-09-23 09:18:17 +0700

    Create login page

--- src/pages/signin/index.js
+++ src/pages/signin/index.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
-import { Form, Card, Container, Button } from 'react-bootstrap';
-// import SButton from '../../components/Button';
+import axios from 'axios';
+
+import { Form, Card, Container } from 'react-bootstrap';
+import TextInputWithLabel from '../../components/TextInputWithLabel';
+import SButton from '../../components/Button';
+import SAlert from '../../components/Alert';
 
 function PageSignin() {
 	const [form, setForm] = useState({
@@ -8,29 +12,78 @@ function PageSignin() {
 		password: '',
 	});
 
+	const [alert, setAlert] = useState({
+		status: false,
+		message: '',
+		type: '',
+	});
+
+	const [isLoading, setIsLoading] = useState(false);
+
 	const handleChange = (e) => {
-		setForm({ ...form.email, [e.target.name]: e.target.value });
+		setForm({ ...form, [e.target.name]: e.target.value });
+	};
+
+	const handleSubmit = async () => {
+		setIsLoading(true);
+
+		try {
+			const res = await axios.post(
+				'http://localhost:9000/api/v1/cms/auth/signin',
+				{
+					email: form.email,
+					password: form.password,
+				}
+			);
+			console.log(res.data.token);
+			setIsLoading(false);
+		} catch (err) {
+			setIsLoading(false);
+			setAlert({
+				status: true,
+				message: err?.response?.data?.msg ?? 'Internal server error',
+
+				type: 'danger',
+			});
+		}
 	};
 
 	return (
-		<Container md={12}>
+		<Container md={12} className='my-5'>
+			<div className='m-auto' style={{ width: '50%' }}>
+				{alert.status && (
+					<SAlert type={alert.type} message={alert.message}></SAlert>
+				)}
+			</div>
 			<Card style={{ width: '50%' }} className='m-auto mt-5'>
 				<Card.Title className='text-center mt-2'>Welcome Back!</Card.Title>
 				<Card.Body>
 					<Form>
-						<Form.Group className='mb-3' controlId='formBasicEmail'>
-							<Form.Label>Email address</Form.Label>
-							<Form.Control name='email' value={form.email} type='email' placeholder='Enter email' onChange={handleChange} />
-						</Form.Group>
+						<TextInputWithLabel
+							label='Email address'
+							name='email'
+							value={form.email || ''}
+							type='email'
+							placeholder='Enter email'
+							onChange={handleChange}
+						/>
 
-						<Form.Group className='mb-3' controlId='formBasicPassword'>
-							<Form.Label>Password</Form.Label>
-							<Form.Control name='password' value={form.password} type='password' placeholder='Password' onChange={handleChange} />
-						</Form.Group>
-						<Form.Group className='mb-3' controlId='formBasicCheckbox'>
-							<Form.Check type='checkbox' label='Keep login' />
-						</Form.Group>
-						<Button variant='primary'>Submit</Button>
+						<TextInputWithLabel
+							label='Password'
+							name='password'
+							value={form.password || ''}
+							type='password'
+							placeholder='Enter password'
+							onChange={handleChange}
+						/>
+
+						<SButton
+							loading={isLoading}
+							disabled={isLoading}
+							// action={handleSubmit}
+							variant='primary'>
+							Submit
+						</SButton>
 					</Form>
 				</Card.Body>
 			</Card>
